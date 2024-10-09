8de9deb5df3515bae3ab03733bb3bf177f9c957c
riaanjlagrange <riaanjlagrange@gmail.com>
2024-10-10 01:25:38 +0200

    fix menu toggle display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
 
   const [menuExpanded, setMenuExpanded] = useState(true);
   const [columnStyling, setColumnStyling] = useState("");
+  const [outletStyling, setOutletStyling] = useState("");
 
   const handleToggleMenu = () => {
     return menuExpanded ? setMenuExpanded(false) : setMenuExpanded(true);
@@ -13,17 +14,19 @@ function App() {
 
   useEffect(() => {
     if (menuExpanded) {
-      setColumnStyling("grid-cols-4")
+      setColumnStyling("grid-cols-4");
+      setOutletStyling("col-span-3");
     } else {
-      setColumnStyling("grid-cols-12")
+      setColumnStyling("grid-cols-12");
+      setOutletStyling("col-span-12");
     }
-  }, [columnStyling, menuExpanded])
+  }, [columnStyling, menuExpanded]);
 
   return (
     <>
       <div className={`grid ${columnStyling} w-[100vw] h-[100vh] dark:bg-slate-900 relative `}>
         <Navbar toggleMenu={handleToggleMenu} isMenuExpanded={menuExpanded} />
-        <div className={menuExpanded ? "col-span-3 flex items-center justify-center" : "col-span-12 flex items-center justify-center"}>
+        <div className={`flex items-center justify-center ${outletStyling}`}>
           <Outlet />
         </div>
       </div>
